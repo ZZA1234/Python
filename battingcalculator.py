a376be599696ad7f573289da96526bc5ba6ef298
ZZA1234 <114941666+ZZA1234@users.noreply.github.com>
2024-04-23 09:01:27 +0530

    Create battingcalculator.py

---
+++ battingcalculator.py
@@ -0,0 +1,10 @@
+r=input("Enter Number of Runs Scored by Batsman :")
+r=int(r)
+
+b=input("Enter Number of Balls Faced by Batsman :")
+b=int(b)
+
+avg=r/b
+sr=avg*100
+
+print("The Batting Strike Rate is :",sr)
